e49e969346970c9fe2021a284e3343251e272c93
ozawa <ozawa>
2002-08-28 08:36:01 +0000

    Java soruces must be layouted by their package hierarchy. Added test class.

---
+++ pgserver/org/montsuqi/PG_Event.java
@@ -0,0 +1,14 @@
+package org.montsuqi;
+
+public class PG_Event {
+
+    public final String window;
+    public final String widget;
+    
+    public PG_Event(String window, String widget) {
+	this.window = window;
+	this.widget = widget;
+    }
+
+    public static final PG_Event NULL_EVENT = new PG_Event("null", "null");
+}
