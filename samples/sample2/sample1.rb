8d3ed254f9353d8f4fa5885e921e092c13ff9ffd
shugo <shugo>
2004-02-01 00:45:07 +0000

    bug fix.

--- samples/sample2/sample1.rb
+++ samples/sample2/sample1.rb
@@ -20,13 +20,14 @@ class Sample1
     adrs = db["adrs"]
     i = 0
     adrs.each("mail", "mail.home" => list.fixed1.key.value) do |item|
-      list.fixed1.clist1.item[i].value1 = adrs.name;
-      list.fixed1.clist1.item[i].value2 = adrs.tel;
-      list.fixed1.clist1.item[i].value3 = adrs.mail.home;
-      if adrs.address
-        list.fixed1.clist1.item[i].value4 = adrs.address[0];
+      list.fixed1.clist1.item[i].value1 = item.name;
+      list.fixed1.clist1.item[i].value2 = item.tel;
+      list.fixed1.clist1.item[i].value3 = item.mail.home;
+      if item.address
+        list.fixed1.clist1.item[i].value4 = item.address[0];
       end
       list.fixed1.clist1.select[i] = false
+      break if i == 19
       i += 1
     end
     list.fixed1.clist1.count = i
