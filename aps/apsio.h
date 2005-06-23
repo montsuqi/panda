3e67b7f8dd7c02dcd8522cf2280f85c0d6dd942e
Masami Ogoshi <ogochan@nurs.or.jp>
2005-06-23 12:14:58 +0000

    correct copyright notice

--- aps/apsio.h
+++ aps/apsio.h
@@ -1,22 +1,21 @@
-/*	PANDA -- a simple transaction monitor
-
+/*
+PANDA -- a simple transaction monitor
 Copyright (C) 2000-2003 Ogochan & JMA (Japan Medical Association).
+Copyright (C) 2004-2005 Ogochan.
 
-This module is part of PANDA.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-	PANDA is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY.  No author or distributor accepts responsibility
-to anyone for the consequences of using it or for whether it serves
-any particular purpose or works at all, unless he says so in writing.
-Refer to the GNU General Public License for full details. 
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-	Everyone is granted permission to copy, modify and redistribute
-PANDA, but only under the conditions described in the GNU General
-Public License.  A copy of this license is supposed to have been given
-to you along with PANDA so you can know your rights and
-responsibilities.  It should be in a file named COPYING.  Among other
-things, the copyright notice and this notice must be preserved on all
-copies. 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 #ifndef	_APS_H
