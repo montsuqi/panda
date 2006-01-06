f9263a89205f9eb2eb1675d4801e58b760f9e900
Noboru Saitou <noborus@netlab.jp>
2006-01-06 09:55:31 +0000

    add include errno.h

--- libs/auth.c
+++ libs/auth.c
@@ -39,6 +39,7 @@ Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #include	<string.h>
 #include	<ctype.h>
 #include	<glib.h>
+#include	<errno.h>
 
 #include	"types.h"
 #include	"libmondai.h"
