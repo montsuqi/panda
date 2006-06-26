a01c768c793ad5fb8261e214d9ce7f90a50c091e
Noboru Saitou <noborus@netlab.jp>
2006-06-26 04:39:42 +0000

    It doesn't make an error of bddir (for compatibility).

--- aps/dbstub_main.c
+++ aps/dbstub_main.c
@@ -128,6 +128,8 @@ static	ARG_TABLE	option[] = {
 		"環境のベースディレクトリ"		 				},
 	{	"record",	STRING,		TRUE,	(void*)&RecordDir,
 		"データ定義格納ディレクトリ"	 				},
+	{	"bddir",	STRING,		TRUE,	(void*)&D_Dir,
+		"定義格納ディレクトリ(obsolete)" 				},
 	{	"ddir",		STRING,		TRUE,	(void*)&D_Dir,
 		"定義格納ディレクトリ"			 				},
 	{	"dir",		STRING,		TRUE,	(void*)&Directory,
