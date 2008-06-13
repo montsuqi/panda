6430f505a1d8c66d2c2f8c663fb0954209c7c1b3
Noboru Saitou <noborus@netlab.jp>
2008-06-13 02:34:54 +0000

    PostgreSQL option sslmode was added.

--- dblib/dbgroup.h
+++ dblib/dbgroup.h
@@ -26,18 +26,17 @@
 
 extern	int		OpenRedirectDB(DBG_Struct *dbg);
 extern	int		CloseRedirectDB(DBG_Struct *dbg);
-extern	int		ExecRedirectDBOP(DBG_Struct *dbg, char *sql, int usage);
+extern	int		ExecRedirectDBOP(DBG_Struct *dbg, char *sql);
 extern	int		TransactionRedirectStart(DBG_Struct *dbg);
 extern	int		TransactionRedirectEnd(DBG_Struct *dbg);
 
 extern	void	CloseDB_RedirectPort(DBG_Struct *dbg);
 
-extern	Port	*GetDB_Port(DBG_Struct *dbg, int usage);
-extern	char	*GetDB_Host(DBG_Struct *dbg, int usage);
-extern	char	*GetDB_PortName(DBG_Struct *dbg, int usage);
-extern	char	*GetDB_DBname(DBG_Struct *dbg, int usage);
-extern	char	*GetDB_User(DBG_Struct *dbg, int usage);
-extern	char	*GetDB_Pass(DBG_Struct *dbg, int usage);
+extern	char	*GetDB_Host(DBG_Struct *dbg);
+extern	char	*GetDB_Port(DBG_Struct *dbg);
+extern	char	*GetDB_DBname(DBG_Struct *dbg);
+extern	char	*GetDB_User(DBG_Struct *dbg);
+extern	char	*GetDB_Pass(DBG_Struct *dbg);
 
 extern	ValueStruct	*_GetDB_Argument(RecordStruct *rec, char *pname, char *func, int *apno);
 extern	void	MakeCTRL(DBCOMM_CTRL *ctrl, ValueStruct *mcp);
@@ -63,6 +62,7 @@ GLOBAL	char	*DB_Port;
 GLOBAL	char	*DB_Name;
 GLOBAL	char	*DB_User;
 GLOBAL	char	*DB_Pass;
+GLOBAL	char	*DB_Sslmode;
 
 GLOBAL	Bool	fNoCheck;
 GLOBAL	Bool	fNoRedirect;
