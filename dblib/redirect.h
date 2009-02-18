c1a6bbb9bb6c1168379f9c5228b1624c815b51d0
Noboru Saitou <noborus@netlab.jp>
2009-02-18 05:42:47 +0000

    correct header name.

--- dblib/redirect.h
+++ dblib/redirect.h
@@ -17,8 +17,8 @@
  * Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#ifndef	_INC_LOG_H
-#define	_INC_LOG_H
+#ifndef	_INC_REDIRECT_H
+#define	_INC_REDIRECT_H
 #include	"dbgroup.h"
 
 #ifndef	PacketClass
