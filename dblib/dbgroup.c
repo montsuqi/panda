ec3c8b50b1430739f3178fefca257870c6498ed9
Noboru Saitou <noborus@netlab.jp>
2010-02-17 07:23:13 +0000

    fix exec_query encoding

--- dblib/dbgroup.c
+++ dblib/dbgroup.c
@@ -69,7 +69,7 @@ SetDBAudit(
 			SetValueInteger(GetItemLongName(audit,"ticket_id"), dbg->ticket_id);
 			lbs = dbg->last_query;
 			if ((lbs != NULL) && (LBS_Size(lbs) > 0)){
-				SetValueString(GetItemLongName(audit,"exec_query"),LBS_Body(lbs), NULL);
+				SetValueString(GetItemLongName(audit,"exec_query"),LBS_Body(lbs), dbg->coding);
 			}
 		}
 	}
