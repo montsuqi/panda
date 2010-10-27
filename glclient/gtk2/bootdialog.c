1d9c7c3c345bc2bdd03560814387c1756448badf
yusuke mihara <mihara@netlab.jp>
2010-10-27 07:09:33 +0000

    * fix change for gtk_panda_clist_set_columns

--- glclient/gtk2/bootdialog.c
+++ glclient/gtk2/bootdialog.c
@@ -438,7 +438,7 @@ server_dialog_new (BDConfig * config, GtkWidget *parent)
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), scroll, TRUE, TRUE, 5);
 
   self->server_list = clist = gtk_panda_clist_new();
-  gtk_panda_clist_set_n_columns(GTK_PANDA_CLIST(clist),server_dialog_titles_count);
+  gtk_panda_clist_set_columns(GTK_PANDA_CLIST(clist),server_dialog_titles_count);
   gtk_panda_clist_set_selection_mode(GTK_PANDA_CLIST(clist),
     GTK_SELECTION_SINGLE);
   for (i = 0; i < server_dialog_titles_count; i++) {
