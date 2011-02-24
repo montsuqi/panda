ff3254063a374b8c6fab92a3949931f4fb83cbd5
Noboru Saitou <noborus@netlab.jp>
2011-02-24 07:07:58 +0000

    Message change aps terminates abnormally

--- wfc/mqthread.c
+++ wfc/mqthread.c
@@ -232,7 +232,7 @@ ENTER_FUNC;
 			break;
 		  default:
 		  badio:
-			MessageLog("aps die ?");
+			Message("apd die [%s@%s] %s:%s:%s", hdr->user, TermToHost(hdr->term), hdr->window, hdr->widget, hdr->event);
 			done = TRUE;
 			flag = 0;
 			break;
