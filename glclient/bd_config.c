7402457e7f4b8ae7f8a9ee0261584bc3de119508
yusuke mihara <mihara@netlab.jp>
2011-10-26 07:04:13 +0000

    * set mlog flase

--- glclient/bd_config.c
+++ glclient/bd_config.c
@@ -643,7 +643,7 @@ static GL_CONFIG_ENTRY conf_entries[] = {
 {"string", "cache",        "$HOME/.glclient/cache"},
 {"string", "style",        ""},
 {"string", "gtkrc",        ""},
-{"bool",   "mlog",         "T"},
+{"bool",   "mlog",         "F"},
 {"bool",   "keybuf",       "F"},
 {"string", "user",         "user"},
 {"bool",   "savepassword", "T"},
