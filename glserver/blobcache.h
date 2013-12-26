6d3c7de0da496230705163a03062f04c810ce18d
yusuke mihara <mihara@netlab.jp>
2013-12-26 09:35:32 +0900

    fix warnings

--- glserver/blobcache.h
+++ glserver/blobcache.h
@@ -33,9 +33,6 @@
 #	define	GLOBAL		extern
 #endif
 
-#undef	GLOBAL
-
-extern	void		SetCacheDir(char* cachedir);
 extern	char		*BlobCacheFileName(ValueStruct *value);
 extern	void		BlobCacheCleanUp();
 extern	size_t		BlobCacheFileSize(ValueStruct *value);
