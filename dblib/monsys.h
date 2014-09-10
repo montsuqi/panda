e5034c10584a8919cac2e4d1c8300b33dfef58d4
Noboru Saito <noborus@netlab.jp>
2014-09-10 12:47:31 +0900

    char -> const charに変更

--- dblib/monsys.h
+++ dblib/monsys.h
@@ -22,6 +22,6 @@
 #include	"dbgroup.h"
 
 extern	DBG_Struct *GetDBG_monsys(void);
-extern	char       *Escape_monsys(DBG_Struct *dbg, char *src);
+extern	char       *Escape_monsys(DBG_Struct *dbg, const char *src);
 
 #endif
