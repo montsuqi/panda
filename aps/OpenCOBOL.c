bfa99dea957a003d304cf0d6e5ebb5912abd5977
Noboru Saito <noborus@netlab.jp>
2014-10-09 18:54:12 +0900

    UnPackValue/PackValueのCacheを止める

--- aps/OpenCOBOL.c
+++ aps/OpenCOBOL.c
@@ -55,41 +55,6 @@ static	void	*ScrData;
 
 static	char	*ModuleLoadPath;
 
-typedef	struct _ScreenCache	{
-	size_t	size;
- 	void	*scr;
-}	ScreenCache;
-
-static	GHashTable		*CacheScreen;
-
-static	size_t
-IsCacheScreen(
-	char	*name,
-	char 	*scr)
-{
-	size_t	ret = 0;
-	ScreenCache	*sch;
-
-	if ((sch = g_hash_table_lookup(CacheScreen,name)) != NULL){
-		if (memcmp(sch->scr, scr, sch->size) == 0){
-			ret = sch->size;
-		}
-	}
-	return ret;
-}
-
-static	void
-AddCacheScreen(
-	char *name,
-	char *scr)
-{
-	ScreenCache	*sch;
-
-	if ((sch = g_hash_table_lookup(CacheScreen, name)) != NULL){
-		memcpy(sch->scr, scr, sch->size);
-	}
-}
-
 static	void
 PutApplication(
 	ProcessNode	*node)
@@ -128,7 +93,6 @@ GetApplication(
 {
 	char	*scr;
 	int		i;
-	size_t	size;
 	long	start
 		,	end;
 
@@ -145,12 +109,7 @@ ENTER_FUNC;
 	}
 	for	( i = 0 , scr = (char *)ScrData ; i < node->cWindow ; i ++ ) {
 		if		(  node->scrrec[i]  !=  NULL  ) {
-			if ((size = IsCacheScreen(node->scrrec[i]->name, scr)) != 0) {
-				scr += size;
-			} else {
-				AddCacheScreen(node->scrrec[i]->name, scr);
-				scr += OpenCOBOL_UnPackValue(OpenCOBOL_Conv,scr,node->scrrec[i]->value);
-			}
+			scr += OpenCOBOL_UnPackValue(OpenCOBOL_Conv,scr,node->scrrec[i]->value);
 		}
 	}
 	end = GetNowTime();
@@ -199,8 +158,7 @@ _ReadyDC(
 {
 	int		i;
 	size_t	scrsize;
-	ScreenCache	*sch;
-	
+
 ENTER_FUNC;
 	OpenCOBOL_Conv = NewConvOpt();
 	ConvSetSize(OpenCOBOL_Conv,ThisLD->textsize,ThisLD->arraysize);
@@ -226,15 +184,10 @@ ENTER_FUNC;
 	if		(  ThisLD->sparec  !=  NULL  ) {
 		SpaData = xmalloc(OpenCOBOL_SizeValue(OpenCOBOL_Conv,ThisLD->sparec->value));
 	}
-	CacheScreen = NewNameHash();
 	scrsize = 0;
 	for	( i = 0 ; i < ThisLD->cWindow ; i ++ ) {
 		if		(  ThisLD->windows[i]  !=  NULL  ) {
-			sch = New(ScreenCache);
-			sch->size = OpenCOBOL_SizeValue(OpenCOBOL_Conv,ThisLD->windows[i]->value);
-			sch->scr = malloc(sch->size);
-			scrsize += sch->size;
-			g_hash_table_insert(CacheScreen,ThisLD->windows[i]->name,sch);
+			scrsize += OpenCOBOL_SizeValue(OpenCOBOL_Conv,ThisLD->windows[i]->value);
 		}
 	}
 	ScrData = xmalloc(scrsize);
