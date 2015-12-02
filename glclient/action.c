b290a41618579b95a7ce20cdd023648fe4694366
yusuke mihara <mihara@netlab.jp>
2015-12-02 16:27:17 +0900

    ReDrawTopWindowをデフォルトで行うよう修正

--- glclient/action.c
+++ glclient/action.c
@@ -253,6 +253,7 @@ static struct changed_hander {
 
 static void ScaleWidget(GtkWidget *widget, gpointer data);
 static void ScaleWindow(GtkWidget *widget);
+static void ReDrawTopWindow(void);
 
 static void
 SetWindowIcon(GtkWindow *window)
@@ -671,6 +672,7 @@ ENTER_FUNC;
 		}
 		SetTitle(TopWindow);
 		SetBGColor(TopWindow);
+		ReDrawTopWindow();
 	} else {
 	dbgmsg("show dialog\n");
 		GtkWidget *parent = TopWindow;
@@ -886,6 +888,24 @@ ScaleWindow(
 #endif
 		gtk_widget_set_size_request(widget,_w,_h); 
 	} 
+}
+
+static	void
+ReDrawTopWindow(void)
+{
+#define WINC (1)
+	static int i = 0;
+	int width,height;
+
+	gtk_window_get_size(GTK_WINDOW(TopWindow),&width,&height);
+    g_signal_handlers_block_by_func(TopWindow,ConfigureWindow,NULL);
+	if (i%2 == 0) {
+		gtk_window_resize(GTK_WINDOW(TopWindow),width, height-WINC);
+	} else {
+		gtk_window_resize(GTK_WINDOW(TopWindow),width, height+WINC);
+	}
+    g_signal_handlers_unblock_by_func(TopWindow,ConfigureWindow,NULL);
+	i++;
 }
 
 extern	void
