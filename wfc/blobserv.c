d44def4b962c18cf97b69deff71848ac708fffa3
yusuke mihara <mihara@netlab.jp>
2017-10-23 09:47:02 +0900

    delete blob in blob_export

--- wfc/blobserv.c
+++ wfc/blobserv.c
@@ -78,7 +78,6 @@ BLOBEXPORT(
 		SendLength(fp,size); 			ON_IO_ERROR(fp,badio);
 		Send(fp,buff,size);
 		xfree(buff);
-		blob_delete(dbg,obj);
 	} else {
 		SendPacketClass(fp,BLOB_NOT);	ON_IO_ERROR(fp,badio);
 	}
