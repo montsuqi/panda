ee87273eec569c8dd3c153ded1cd42d41c10f7d2
yusukemihara <mihara@netlab.jp>
2018-07-25 09:59:04 +0900

    fix warning

--- glserver/http.c
+++ glserver/http.c
@@ -229,7 +229,7 @@ void SendResponse(HTTP_REQUEST *req, int status, char *body, size_t body_size,
     Send(req->fp, (char *)str, strlen(str));
   }
 
-  va_start(ap, NULL);
+  va_start(ap, body_size);
   while (1) {
     h = va_arg(ap, char *);
     if (h == NULL) {
