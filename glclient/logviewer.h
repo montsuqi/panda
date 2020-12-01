f4f45651a9848c5937a0fc6b2d4069a8c275c7e5
yusukemihara <yusuke.mihara@nacl-med.co.jp>
2020-12-01 13:24:13 +0900

    Merge pull request #270 from montsuqi/add_logviewer2
    ログビューア追加

---
+++ glclient/logviewer.h
@@ -0,0 +1,41 @@
+/*
+ * PANDA -- a simple transaction monitor
+ * Copyright (C) 2004-2008 Kouji TAKAO & JMA (Japan Medical Association).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef __LOGVIEWER_H_INCLUDED__
+#define __LOGVIEWER_H_INCLUDED__
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <glib.h>
+
+void LogViewerRun();
+
+#endif /* #ifndef __LOGVIEWER_H_INCLUDED__ */
+
+/*************************************************************
+ * Local Variables:
+ * mode: c
+ * c-set-style: gnu
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * End:
+ ************************************************************/
