24eee7f15f8a44dec30f567eb4f0a8c8ddddd9dd
yusuke mihara <yusuke.mihara@nacl-med.co.jp>
2021-03-17 10:24:06 +0900

    1日以上経過したセッションデータを削除するよう修正

--- wfc/termthread.c
+++ wfc/termthread.c
@@ -142,13 +142,8 @@ static void FreeSessionData(SessionData *data) {
   if (data->agent != NULL) {
     xfree(data->agent);
   }
-#if 0
-	if (!getenv("WFC_KEEP_TEMPDIR")) {
-		if (!rm_r(data->hdr->tempdir)) {
-			Error("cannot remove session tempdir %s",data->hdr->tempdir);
-		}
-	}
-#endif
+  /* セッションデータの削除 */
+  rm_r_old_depth(TempDirRoot,86400,1); /* 86400 = 1day */
   xfree(data->hdr);
   g_hash_table_foreach_remove(data->spadata, (GHRFunc)FreeSpa, NULL);
   DestroyHashTable(data->spadata);
